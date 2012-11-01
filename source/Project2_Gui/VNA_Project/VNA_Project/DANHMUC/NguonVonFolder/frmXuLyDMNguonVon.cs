38e46436189763e33f0564139663361e118c0b1d
mr.k29vn <Mr.k29vn@gmail.com>
2012-11-01 04:32:27 +0000

    DM Nguồn Vốn FULL

---
+++ source/Project2_Gui/VNA_Project/VNA_Project/DANHMUC/NguonVonFolder/frmXuLyDMNguonVon.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using VNA_Project.Entity;
+using VNA_Project.BIZ;
+using Qios.DevSuite.Components;
+
+namespace VNA_Project.DANHMUC.NguonVonFolder
+{
+    public partial class frmXuLyDMNguonVon : Qios.DevSuite.Components.Ribbon.QRibbonForm 
+    {
+        #region khai báo
+        bool Them = true;
+        #endregion
+
+        public frmXuLyDMNguonVon()
+        {//Thêm
+            InitializeComponent();
+        }
+        public frmXuLyDMNguonVon(DataGridViewRow dgvr)
+        {//Sửa
+            InitializeComponent();
+            try
+            {
+                Them = false;
+                NguonVon temp = Utils.DataGridViewRow_to_NguonVon(dgvr);
+                txtMaNguonVon.Text = temp.MaNguonVon;
+                txtMaNguonVon.Enabled = false;
+                txtTenNguonVon.Text = temp.TenNguonVon;
+            }
+            catch { }
+        }
+
+        private void btnDongY_Click(object sender, EventArgs e)
+        {
+            if (Them)
+            {//Thêm
+                NguonVon temp = new NguonVon();
+                temp.MaNguonVon = txtMaNguonVon.Text;
+                temp.TenNguonVon = txtTenNguonVon.Text;
+                int kq = NguonVonBiz.AddNguonVon(temp);
+                if (kq > 0) MSG.ThemThanhCong();
+                else MSG.ThemThatBai();
+            }
+            else
+            {//Sửa
+                NguonVon temp = new NguonVon();
+                temp.MaNguonVon = txtMaNguonVon.Text;
+                temp.TenNguonVon = txtTenNguonVon.Text;
+                int kq = NguonVonBiz.EditNguonVon(temp);
+                if (kq > 0) MSG.SuaThanhCong();
+                else MSG.SuaThatBai();
+            }
+            this.Close();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
