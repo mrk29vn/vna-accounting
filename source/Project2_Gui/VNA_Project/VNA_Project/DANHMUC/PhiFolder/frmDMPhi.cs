15d7748fd5ada4f1af2708dda96bb426447d9232
mr.k29vn <Mr.k29vn@gmail.com>
2012-11-04 07:57:29 +0000

    DM Phi Full

---
+++ source/Project2_Gui/VNA_Project/VNA_Project/DANHMUC/PhiFolder/frmDMPhi.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using VNA_Project.Entity;
+using VNA_Project.BIZ;
+using Qios.DevSuite.Components;
+using System.Linq;
+
+namespace VNA_Project.DANHMUC.PhiFolder
+{
+    public partial class frmDMPhi : Qios.DevSuite.Components.Ribbon.QRibbonForm
+    {
+        #region khai báo
+        public static List<Phi> Ldata = new List<Phi>();
+        int vtIndex = 0;    //vị trí dòng cell đang được chọn trong datagridview
+        #endregion
+
+        public frmDMPhi()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLoad(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            Them();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            Sua();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            Xoa();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #region Function
+        void FixDataGirdView()
+        {
+            FixDataGirdView(DataGridView);
+        }
+        void FixDataGirdView(System.Windows.Forms.DataGridView DataGridView)
+        {
+            for (int j = 1; j < DataGridView.ColumnCount; j++) DataGridView.Columns[j].Visible = false;
+            DataGridView.ReadOnly = true;
+
+            DataGridView.Columns["MaPhi"].HeaderText = "Mã phí";
+            DataGridView.Columns["TenPhi"].HeaderText = "Tên phí";
+
+            DataGridView.Columns["MaPhi"].Visible = true;
+            DataGridView.Columns["TenPhi"].Visible = true;
+
+            DataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DataGridView.AllowUserToAddRows = false;
+            DataGridView.AllowUserToDeleteRows = false;
+            DataGridView.AllowUserToResizeRows = false;
+            DataGridView.RowHeadersVisible = false;
+        }
+        #endregion
+
+        #region Nghiệp vụ
+        private void HienThi()
+        {
+            Ldata = PhiBiz.getListPhi();
+            DataGridView.DataSource = Ldata.ToArray();
+            FixDataGirdView();
+        }
+        private void Them()
+        {
+            try
+            {
+                frmXuLyDMPhi frm = new frmXuLyDMPhi();
+                frm.StartPosition = FormStartPosition.CenterScreen;
+                frm.ShowDialog();
+                HienThi();
+                if (Ldata.Count != 0) DataGridView.Rows[vtIndex == -1 ? 0 : vtIndex].Selected = true;   //nếu ko có phần tử nào thì thôi
+            }
+            catch (Exception ex)
+            {
+                MSG.Error(ex);
+            }
+        }
+        private void Sua()
+        {
+            try
+            {
+                if (vtIndex != -1)  //khi click lên tiêu đề header của datagrid thì bỏ qua
+                {
+                    frmXuLyDMPhi frm = new frmXuLyDMPhi(DataGridView.Rows[vtIndex]);
+                    frm.StartPosition = FormStartPosition.CenterScreen;
+                    frm.ShowDialog();
+                    HienThi();
+                    if (Ldata.Count != 0) DataGridView.Rows[vtIndex == -1 ? 0 : vtIndex].Selected = true;   //nếu ko có phần tử nào thì thôi
+                }
+            }
+            catch (Exception ex)
+            {
+                MSG.Error(ex);
+            }
+        }
+        private void Xoa()
+        {
+            try
+            {
+                if (vtIndex != -1)  //khi click lên tiêu đề header của datagrid thì bỏ qua
+                {
+                    if (MSG.BanCoChacChanMuonXoaKhong() == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        Phi temp = Utils.DataGridViewRow_to_Phi(DataGridView.Rows[vtIndex]);
+                        int kq = PhiBiz.DeletePhi(temp);
+                        //if (kq > 0) MSG.XoaThanhCong();
+                        //else MSG.XoaThatBai();
+                        if (kq <= 0) MSG.XoaThatBai();
+                        HienThi();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MSG.Error(ex);
+            }
+        }
+        #endregion
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {//Khi click vào Datagridview thì lấy vị trí của dòng vừa click vào và lưu lại vào biến vtIndex
+            vtIndex = e.RowIndex;
+        }
+        private void DataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Sua();
+        }
+
+        private void txtMaSearch_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Phi> Ltemp = new List<Phi>();
+                string search = txtMaSearch.Text.ToUpper();
+                foreach (Phi item in Ldata)
+                {
+                    if (item.MaPhi.ToUpper().Contains(search)) Ltemp.Add(item);
+                }
+                DataGridView.DataSource = Ltemp.ToArray();
+                FixDataGirdView();
+            }
+            catch (Exception ex)
+            {
+                MSG.Error(ex);
+            }
+        }
+
+        private void txtTenSearch_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Phi> Ltemp = new List<Phi>();
+                string search = txtTenSearch.Text.ToUpper();
+                foreach (Phi item in Ldata)
+                {
+                    if (item.TenPhi.ToUpper().Contains(search)) Ltemp.Add(item);
+                }
+                DataGridView.DataSource = Ltemp.ToArray();
+                FixDataGirdView();
+            }
+            catch (Exception ex)
+            {
+                MSG.Error(ex);
+            }
+        }
+    }
+}
---
+++ source/Project2_Gui/VNA_Project/VNA_Project/DANHMUC/PhiFolder/frmXuLyDMPhi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using VNA_Project.Entity;
+using VNA_Project.BIZ;
+using Qios.DevSuite.Components;
+
+namespace VNA_Project.DANHMUC.PhiFolder
+{
+    public partial class frmXuLyDMPhi : Qios.DevSuite.Components.Ribbon.QRibbonForm
+    {
+        #region khai báo
+        bool Them = true;
+        #endregion
+
+        public frmXuLyDMPhi()
+        {//Thêm
+            InitializeComponent();
+        }
+        public frmXuLyDMPhi(DataGridViewRow dgvr)
+        {//Sửa
+            InitializeComponent();
+            try
+            {
+                Them = false;
+                Phi temp = Utils.DataGridViewRow_to_Phi(dgvr);
+                txtMa.Text = temp.MaPhi;
+                txtMa.Enabled = false;
+                txtTen.Text = temp.TenPhi;
+            }
+            catch { }
+        }
+
+        private void btnDongY_Click(object sender, EventArgs e)
+        {
+            if (Them)
+            {//Thêm
+                Phi temp = new Phi();
+                temp.MaPhi = txtMa.Text;
+                temp.TenPhi = txtTen.Text;
+
+                if (!CheckLoi(temp)) return;
+
+                int kq = PhiBiz.AddPhi(temp);
+                if (kq > 0) MSG.ThemThanhCong();
+                else MSG.ThemThatBai();
+            }
+            else
+            {//Sửa
+                Phi temp = new Phi();
+                temp.MaPhi = txtMa.Text;
+                temp.TenPhi = txtTen.Text;
+                int kq = PhiBiz.EditPhi(temp);
+                if (kq > 0) MSG.SuaThanhCong();
+                else MSG.SuaThatBai();
+            }
+            this.Close();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        bool CheckLoi(Phi data)
+        {
+            bool kq = true;
+            //mã phí rỗng
+            if (string.IsNullOrEmpty(data.MaPhi))
+            {
+                MSG.ErrorStand("Bạn chưa nhập mã phí!");
+                txtMa.Focus();
+                return false;
+            }
+            //mã phí đã có trong cơ sở dữ liệu
+            foreach (Phi item in frmDMPhi.Ldata)
+            {
+                if (item.MaPhi.ToUpper().Equals(txtMa.Text.ToUpper()))
+                {
+                    MSG.ErrorStand("Mã phí đã có trong cơ sở dữ liệu!");
+                    txtMa.Focus();
+                    return false;
+                }
+            }
+            return kq;
+        }
+    }
+}
