53c1d9b3fea279b6e2938bbb577c8613c6626769
mr.k29vn <Mr.k29vn@gmail.com>
2013-02-01 19:00:22 +0000

    Fix Lay_ID

--- source2/SuperMarketManagerment/BO/Lay_ID.cs
+++ source2/SuperMarketManagerment/BO/Lay_ID.cs
@@ -30,27 +30,46 @@ namespace BizLogic
             cmd.Parameters.Add("@table", SqlDbType.NVarChar, 50).Value = lid1.TenBang;
             SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
 
+            ////Insert Category into ArrayList
+            //ArrayList arr = new ArrayList();
+            //while (dr.Read())
+            //{
+            //    Entities.LayID lid = new Entities.LayID();
+            //    lid.ID = dr["ID"].ToString();
+            //    arr.Add(lid);
+            //}
+            //int n = arr.Count;
+            //if (n == 0) return null;
+
+            //Entities.LayID arrC = new Entities.LayID();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arrC = (Entities.LayID)arr[i];
+            //}
+
+            ////Giai phong bo nho
+            //arr = null;
+
             //Fix 14/01/2013
             string phandau = string.Empty;
             string max = "0";
             while (dr.Read())
             {
-                    string[] temp = dr["ID"].ToString().Split('_');
-                    if (string.IsNullOrEmpty(phandau)) phandau = temp[0];
-                    if (max.Equals("0")) max = temp[1];
-                    try 
-                    {
-                        int ss = int.Parse(temp[1]);
-                        int _max = int.Parse(max);
-                        max = _max < ss ? temp[1] : max; 
-                    }
-                    catch { }
+                string[] temp = dr["ID"].ToString().Split('_');
+                if (string.IsNullOrEmpty(phandau)) phandau = temp[0];
+                if (max.Equals("0")) max = temp[1];
+                try
+                {
+                    int ss = int.Parse(temp[1]);
+                    int _max = int.Parse(max);
+                    max = _max < ss ? temp[1] : max;
+                }
+                catch { }
             }
             Entities.LayID arrC = new Entities.LayID();
             arrC.ID = phandau + "_" + max;
             //End Fix 14/01/2013
 
-            //Giai phong bo nho
             cmd.Connection.Dispose();
             cn.Close();
             conn.closeConnection();
