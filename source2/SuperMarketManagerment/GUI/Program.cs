d5750380ebb791da75420eaf260de6671825ae6f
mr.k29vn <Mr.k29vn@gmail.com>
2013-11-16 14:43:37 +0000

    Sửa chút file Program cho gọn code hơn

--- source2/SuperMarketManagerment/GUI/Program.cs
+++ source2/SuperMarketManagerment/GUI/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using GUI;
-using System.Diagnostics;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Net.Sockets;
 
-namespace WindowsFormsApplication1
+namespace GUI
 {
     static class Program
     {
@@ -20,37 +13,8 @@ namespace WindowsFormsApplication1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Process curr = Process.GetCurrentProcess();
-            //Process[] curarr = Process.GetProcessesByName(curr.ProcessName);
-            //bool run=true;
-            //foreach (Process item in curarr)
-            //{
-            //    if (item.Id != curr.Id && item.MainModule.FileName == curr.MainModule.FileName)
-            //    {
-            //        run = false;
-            //    }
-            //}
-            //if (run)
-                Application.Run(new frmLoad());
-            //else
-            //{
-            //    MessageBox.Show("chương trình đang chạy");
-            //    Application.Exit();
-            //}
-            //try
-            //{
-            //    Server_Client.Client cl = new Server_Client.Client();
-            //    // gán TCPclient
-            //    TcpClient client1 = cl.Connect(Luu.IP, Luu.Ports);
-            //    NetworkStream clientstrem = cl.SerializeObj(client1, "LogOut", frmDangNhap.User);
-            //    // đổ mảng đối tượng vào datagripview       
-            //    cl.DeserializeHepper(clientstrem, null);
-            //}
-            //catch (Exception){}
-            //finally
-            //{
-                Environment.Exit(-1);
-            //}
+            Application.Run(new frmLoad());
+            Environment.Exit(-1);
         }
     }
 }
